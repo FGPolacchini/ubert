b1a63c2163fccc91893b8222a497d885a68382e7
Felipe Gallasso Polacchini <fgpolacchini@gmail.com>
2025-10-04 23:45:39 +0200

    Fixed problems with Timer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,20 +5,22 @@ import type { SessionState } from "./utils/sessionManagement";
 import Timer from "./components/breakNudge";
 
 function App() {
-	const [sessionData, useSessionData] = useState<SessionState>();'-'
+	const [sessionData, useSessionData] = useState<SessionState>();
 
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<Card>
-				<CardHeader>This is our app now</CardHeader>
-				<CardDescription>More text just to be sure</CardDescription>
-			</Card>
-			<Timer
-				durationInit={10}
-				durationLong={8}
-				durationShort={3}
-				maxTimeElapsed={20}
-			/>
+			<div className="flex h-full w-full-p-8">
+				<Card>
+					<CardHeader>This is our app now</CardHeader>
+					<CardDescription>More text just to be sure</CardDescription>
+				</Card>
+				<Timer
+					durationInit={10}
+					durationLong={8}
+					durationShort={3}
+					maxTimeElapsed={20}
+				/>
+			</div>
 		</ThemeProvider>
 	);
 }
