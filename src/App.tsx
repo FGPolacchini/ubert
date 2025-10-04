5a3c4d8ca53317a5a201549ade19533b6232a690
Alex Ion <a.g.ion@student.tudelft.nl>
2025-10-04 23:11:30 +0200

    Added timer functionality with customizable times

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { Card, CardDescription, CardHeader } from "./components/ui/card";
+//import { Card, CardDescription, CardHeader } from "./components/ui/card";
 import { useState } from "react";
+import Timer from "./components/breakNudge"
 import type { SessionState } from "./utils/sessionManagement";
 
 function App() {
@@ -8,10 +9,7 @@ function App() {
 
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<Card>
-				<CardHeader>This is our app now</CardHeader>
-				<CardDescription>More text just to be sure</CardDescription>
-			</Card>
+			<Timer durationInit={10} durationLong={8} durationShort={3} maxTimeElapsed={20}></Timer>
 		</ThemeProvider>
 	);
 }
