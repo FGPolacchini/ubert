0ad73c224467ac464063b9078815f145d8c64d98
IsagiMendici <mendea.ianis@gmail.com>
2025-10-05 04:31:28 +0200

    Weather information can now be gathered on an hourly basis

--- server/src/routes/weatherRoutes.ts
+++ server/src/routes/weatherRoutes.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
-import { fetchWeather } from "../weather/weatherAPI";
+import { fetchWeatherHourly, fetchWeatherNow } from "../weather/weatherAPI";
 
 const weatherRoutes = Router();
 
 // I expect to receive a latitude and a longitude in the request body
+// Hour has to be in 24h format
 
 weatherRoutes.get("/info/:latitude/:longitude", (req, res) => {
 	try {
 		const latitude = req.params.latitude;
 		const longitude = req.params.longitude;
-		const weather = fetchWeather(
+		const weather = fetchWeatherNow(
 			latitude,
 			longitude,
 			process.env.WEATHER_API_KEY!
@@ -25,5 +26,33 @@ weatherRoutes.get("/info/:latitude/:longitude", (req, res) => {
 		res.status(500).json({ error: (err as Error).message });
 	}
 });
+
+weatherRoutes.get("/info/hourly/:latitude/:longitude/:hour/:day", (req, res) => {
+	try {
+		const latitude = req.params.latitude;
+		const longitude = req.params.longitude;
+		const hour = req.params.hour;
+		const day = req.params.day;
+		const dayValue = day ?? "1";
+		const weather = fetchWeatherHourly(
+			latitude,
+			longitude,
+			+hour,
+			+dayValue,
+			process.env.WEATHER_API_KEY!
+		).then(
+			(resolved) => {
+				res.json(resolved);
+			},
+			(resolved) => {
+				throw new Error("Json conversion from API not successful");
+			}
+		);
+	} catch (err) {
+		res.status(500).json({ error: (err as Error).message });
+	}
+});
+
+
 
 export default weatherRoutes;
